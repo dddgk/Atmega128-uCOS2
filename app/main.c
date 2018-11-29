c5b0ce53e11a7d9eceeff0f10b08ad3f33f26e31
rueng564 <rueng564@naver.com>
2018-11-29 14:24:43 +0900

    FND 수정, 온도센서 구현중

--- app/main.c
+++ app/main.c
@@ -4,12 +4,15 @@
 #include <util/delay.h>
 
 #define  TASK_STK_SIZE  OS_TASK_DEF_STK_SIZE
-#define  N_TASKS        6
+#define  N_TASKS        8
 
 OS_STK       TaskStk[N_TASKS][TASK_STK_SIZE];
-OS_EVENT	  *Mbox;
+OS_EVENT *Mbox;
 OS_EVENT* mutex;
+OS_EVENT* MsgQ;
 OS_FLAG_GRP* t_grp;		//task의 실행 순서를 정하는 EventFlag
+void* MsgQTbl[2];  //Message Queue Table
+
 // 0: LED, 1: 버저, 2: 광, 3: 온도
 
 volatile INT8U	FndNum;
@@ -18,10 +21,11 @@ volatile INT8U TaskNum;
 INT8U const myMapTbl[] = {0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80};
 unsigned char FNDData[4]={0x3F,0x79,0x38,0};
 void FndTask(void *data);
-void  FndDisplayTask(void *data);
+void FndDisplayTask(void *data);
 void LedTask (void *data);
 void BuzzerTask(void* data);
 void TemperatureTask(void *data);
+void ReadTemperatureTask(void *data);
 void LightTask(void* data);
 void InitI2C();
 int ReadTemperature(void);
@@ -51,18 +55,23 @@ int main (void)
 	Mbox=OSMboxCreate((void*)0);
 	mutex=OSMutexCreate(1,&err);
 	t_grp=OSFlagCreate(myMapTbl[TaskNum],&err);
+	MsgQ=OSQCreate(MsgQTbl,2);
 	OSTaskCreate(TemperatureTask, (void *)0, (void *)&TaskStk[0][TASK_STK_SIZE - 1], 2);
-	OSTaskCreate(LightTask, (void *)0, (void *)&TaskStk[1][TASK_STK_SIZE - 1], 3);
-	OSTaskCreate(LedTask, (void *)0, (void *)&TaskStk[2][TASK_STK_SIZE - 1], 4);
-	OSTaskCreate(BuzzerTask, (void *)0, (void *)&TaskStk[3][TASK_STK_SIZE - 1], 5);
-	OSTaskCreate(FndDisplayTask, (void *)0, (void *)&TaskStk[4][TASK_STK_SIZE - 1], 6);
-	OSTaskCreate(FndTask, (void *)0, (void *)&TaskStk[5][TASK_STK_SIZE - 1], 7);
+	OSTaskCreate(ReadTemperatureTask, (void *)0, (void *)&TaskStk[1][TASK_STK_SIZE - 1], 3);
+	OSTaskCreate(LightTask, (void *)0, (void *)&TaskStk[2][TASK_STK_SIZE - 1], 4);
+	OSTaskCreate(LedTask, (void *)0, (void *)&TaskStk[3][TASK_STK_SIZE - 1], 5);
+	OSTaskCreate(BuzzerTask, (void *)0, (void *)&TaskStk[4][TASK_STK_SIZE - 1], 6);
+	OSTaskCreate(FndDisplayTask, (void *)0, (void *)&TaskStk[5][TASK_STK_SIZE - 1], 6);
+	OSTaskCreate(FndTask, (void *)0, (void *)&TaskStk[6][TASK_STK_SIZE - 1], 7);
 
 	OSStart();
 
 	return 0;
 }
-
+//온도 측정한거 받아서 FNDData 수정하는 Task 우선순위 높음
+//온도 측정 Task 우선순위 낮음
+//메시지 큐로 2개 데이터 (low, high)보내줌
+//받는 Task가 2개 data pend해서 합침
 /*
 SW1: Task Switching용, 한번 누를때마다 LED->버저->광->온도 순으로 Cycle
 FND에는 LED, BUZZ, 빛 정도, 온도 표시
@@ -160,7 +169,7 @@ void FndDisplayTask (void *data)
 	INT8U err;
     data = data;
     DDRC = 0xff;	
-    DDRG = 0x0f;	
+    DDRG = 0x0f;
 	struct Mail* fnddata;
     while(1)  {
 		fnddata=(struct Mail*)OSMboxPend(Mbox,0,&err);
@@ -261,18 +270,69 @@ void LightTask(void* data) {
 	}
 }
 
+void ReadTemperatureTask (void *data)
+{
+	const char digit[12]={0x3f,0x06,0x5b,0x4f,0x66,0x6d,0x7c,0x07,0x7f,0x67,0x40,0x00};
+	INT8U err;
+	data = data;
+	INT8U low,high;
+	int value;
+	INT8S value_int,value_deci;
+	while (1)  {
+		low=*(INT8U*)OSQPend(MsgQ,0,&err);
+		high=*(INT8U*)OSQPend(MsgQ,0,&err);
+		value=(high<<8+low);
+		OSMutexPend(mutex,0,&err);
+		if((value&0x8000) != 0x8000) {
+			FNDData[3]=digit[11];
+		} else {
+			FNDData[3]=digit[10];
+			value=(~value)-1;
+		}
+		value_int=((value&0x7f00)>>8);
+		value_deci=(value&0x00ff);
+		FNDData[2]=digit[(value_int/10)%10];
+		FNDData[1]=(digit[value_int%10]|0x80);
+		FNDData[0]=digit[((value_deci&0x80)==0x80)*5];
+		OSMutexPost(mutex);
+	}
+}
+
 void TemperatureTask (void *data)
 {
-	int	value;
 	INT8U err;
+	INT8U low,high;
 	data = data;
 	InitI2C();
 
 	while (1)  {
 		OSFlagPend(t_grp,0x08,OS_FLAG_WAIT_SET_ALL,0,&err);
 		OS_ENTER_CRITICAL();
-		value = ReadTemperature();
+
+		TWCR = _BV(TWSTA) | _BV(TWINT) | _BV(TWEN);	// START 전송
+		while(!(TWCR & _BV(TWINT)));				// ACK를 기다림
+
+		TWDR = 0x98 + 1; 							//TEMP_I2C_ADDR + 1 > SLA+R 준비, R=1
+		TWCR = _BV(TWINT) | _BV(TWEN);				// SLA+R 전송
+		while(!(TWCR & _BV(TWINT)));				// ACK를 기다림
+
+		TWCR = _BV(TWINT) | _BV(TWEN) | _BV(TWEA);	// 1st DATA 준비
+		while(!(TWCR & _BV(TWINT)));				// ACK를 기다림
+
+		//온도센서는 16bit 기준으로 값을 가져오므로
+		//8비트씩 2번을 받아야 한다.
+		high |= TWDR;								// 1 byte DATA 수신
+		TWCR = _BV(TWINT) | _BV(TWEN); 				// SLA+R 전송
+		while(!(TWCR & _BV(TWINT)));				// ACK를 기다림
+
+		low |= TWDR;								// 1 byte DATA 수신
+		TWCR = _BV(TWINT) | _BV(TWEN) | _BV(TWSTO);	// STOP 전송
+
+		TIMSK = (((high<<8)+low) >= 33) ? TIMSK | _BV(TOIE2): TIMSK & ~_BV(TOIE2);
+
 		OS_EXIT_CRITICAL();
+		OSQPost(MsgQ,&low);
+		OSQPost(MsgQ,&high);
 
 	// value post
 
